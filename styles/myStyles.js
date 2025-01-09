e775b8e4e59f89761eb5342c03ab574fec805a0c
PNNY <panya.naknoppakun@capco.com>
2025-01-09 23:53:43 +0700

    init demo app react native

---
+++ styles/myStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const myStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf:'center'
+  },
+  body: {
+    fontSize: 24,
+    color: 'blue'
+  },
+  imagelogo: {
+    width: 100,
+    height: 100,
+  },
+});
